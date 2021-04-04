4beb45e3f289f4eac44fcd9321ba4a4b1038f124
Alysson Cordeiro <Alysson Cordeiro@PC-ALYSSON>
2021-04-04 19:07:59 -0300

    Criação de procedimento: Cálculo para realização de custo (R$) de fonte de fósforo para ser aplicado por hectare

--- src/main/java/com/mycompany/solo_alysson/Fosforo.java
+++ src/main/java/com/mycompany/solo_alysson/Fosforo.java
@@ -28,12 +28,13 @@ public class Fosforo {
 
     public void calculos() {
         qtdAplicar = 22.31 * 100 / 18;
+        custo = 1260 * qtdAplicar / 1000;
     }
 
     public void recuperacao() {
         switch (fonte) {
             case "Superfosfato simples":
-                System.out.println("Quantidade a aplicar: " + qtdAplicar + "kg/hectare\nCusto - R$/ha " + custo);
+                System.out.println("Quantidade de Superfosfato simples a aplicar: " + qtdAplicar + " kg/hectare\nCusto/hectare: R$" + custo);
                 break;
             default:
                 System.out.println("Fonte de Fosforo incorreta");
