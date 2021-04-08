f6f6486a97daf6b76833e9d10484eb0a7034b79f
Alysson Cordeiro <Alysson Cordeiro@PC-ALYSSON>
2021-04-08 00:15:04 -0300

    Validação de testes Valores ideais dos nutrientes.

--- src/test/java/com/mycompany/solo_alysson/NewEmptyJUnitTest.java
+++ src/test/java/com/mycompany/solo_alysson/NewEmptyJUnitTest.java
@@ -6,6 +6,43 @@ import static org.junit.jupiter.api.Assertions.*;
 public class NewEmptyJUnitTest {
 
     @Test
+    public void testaValoresIdeaisArgiloso() {
+        assertEquals(new Nutrientes(9.0, 0.35, 6.0, 1.5, 9.0, 0.0, 0.0).getFosforo(), 
+                TexturadoSolo.ARGILOSO.calculaValorIdeal().getFosforo());
+        assertEquals(new Nutrientes(9.0, 0.35, 6.0, 1.5, 9.0, 0.0, 0.0).getAluminio(), 
+                TexturadoSolo.ARGILOSO.calculaValorIdeal().getAluminio());
+        assertEquals(new Nutrientes(9.0, 0.35, 6.0, 1.5, 9.0, 0.0, 0.0).getAluminioHidrogenio(), 
+                TexturadoSolo.ARGILOSO.calculaValorIdeal().getAluminioHidrogenio());
+        assertEquals(new Nutrientes(9.0, 0.35, 6.0, 1.5, 9.0, 0.0, 0.0).getCalcio(), 
+                TexturadoSolo.ARGILOSO.calculaValorIdeal().getCalcio());
+        assertEquals(new Nutrientes(9.0, 0.35, 6.0, 1.5, 9.0, 0.0, 0.0).getEnxofre(), 
+                TexturadoSolo.ARGILOSO.calculaValorIdeal().getEnxofre());
+        assertEquals(new Nutrientes(9.0, 0.35, 6.0, 1.5, 9.0, 0.0, 0.0).getMagnesio(), 
+                TexturadoSolo.ARGILOSO.calculaValorIdeal().getMagnesio());
+        assertEquals(new Nutrientes(9.0, 0.35, 6.0, 1.5, 9.0, 0.0, 0.0).getPotassio(), 
+                TexturadoSolo.ARGILOSO.calculaValorIdeal().getPotassio());
+    }
+
+    @Test
+    public void testaValoresIdeaisTexturaMedia() {
+        assertEquals(new Nutrientes(12.0, 0.25, 3.0, 1.0, 6.0, 0.0, 0.0).getFosforo(), 
+                TexturadoSolo.TEXTURA_MEDIA.calculaValorIdeal().getFosforo());
+        assertEquals(new Nutrientes(12.0, 0.25, 3.0, 1.0, 6.0, 0.0, 0.0).getAluminio(), 
+                TexturadoSolo.TEXTURA_MEDIA.calculaValorIdeal().getAluminio());
+        assertEquals(new Nutrientes(12.0, 0.25, 3.0, 1.0, 6.0, 0.0, 0.0).getAluminioHidrogenio(), 
+                TexturadoSolo.TEXTURA_MEDIA.calculaValorIdeal().getAluminioHidrogenio());
+        assertEquals(new Nutrientes(12.0, 0.25, 3.0, 1.0, 6.0, 0.0, 0.0).getCalcio(), 
+                TexturadoSolo.TEXTURA_MEDIA.calculaValorIdeal().getCalcio());
+        assertEquals(new Nutrientes(12.0, 0.25, 3.0, 1.0, 6.0, 0.0, 0.0).getEnxofre(), 
+                TexturadoSolo.TEXTURA_MEDIA.calculaValorIdeal().getEnxofre());
+        assertEquals(new Nutrientes(12.0, 0.25, 3.0, 1.0, 6.0, 0.0, 0.0).getMagnesio(), 
+                TexturadoSolo.TEXTURA_MEDIA.calculaValorIdeal().getMagnesio());
+        assertEquals(new Nutrientes(12.0, 0.25, 3.0, 1.0, 6.0, 0.0, 0.0).getPotassio(), 
+                TexturadoSolo.TEXTURA_MEDIA.calculaValorIdeal().getPotassio());
+
+    }
+    
+    @Test
     public void testaCalculaSCmol() {
         assertEquals(7.54,
                 new Nutrientes().calculaSCmol(0.15, 5.76, 1.63));
@@ -45,7 +82,7 @@ public class NewEmptyJUnitTest {
     @Test
     public void testaCalculaPotassioExistente() {
         assertEquals(1.1636927851047323,
-                new Potassio().calculaPotassioExistente(0.15,
+                new RecuperacaoPotassio().calculaPotassioExistente(0.15,
                         new Nutrientes().calculaCTCCmol(5.35, 7.54))
         );
     }
@@ -53,102 +90,102 @@ public class NewEmptyJUnitTest {
     @Test
     public void testaCalculaPotassioAdicionar() {
         assertEquals(0.2367,
-                new Potassio().calculaPotassioAdicionar(0.15, 1.1636927851047323, 3.0)
+                new RecuperacaoPotassio().calculaPotassioAdicionar(0.15, 1.1636927851047323, 3.0)
         );
     }
 
     @Test
     public void testaConverteCmolcEmMg() {
         assertEquals(92.5497,
-                new Potassio().converteCmolcEmMg(0.2367)
+                new RecuperacaoPotassio().converteCmolcEmMg(0.2367)
         );
     }
 
     @Test
     public void testaConverteMgEmKg() {
         assertEquals(185.0994,
-                new Potassio().converteMgEmKg(92.5497)
+                new RecuperacaoPotassio().converteMgEmKg(92.5497)
         );
     }
 
     @Test
     public void testaConvertePotassioEmOxidoPotassio() {
         assertEquals(222.11928,
-                new Potassio().convertePotassioEmOxidoPotassio(185.0994)
+                new RecuperacaoPotassio().convertePotassioEmOxidoPotassio(185.0994)
         );
     }
 
     @Test
     public void testaCalculaEficiênciaPotassio() {
         assertEquals(261.3168,
-                new Potassio().calculaEficiênciaPotassio(222.11928, 85)
+                new RecuperacaoPotassio().calculaEficiênciaPotassio(222.11928, 85)
         );
     }
 
     @Test
     public void testaCalculaQtdAplicarFontePotassio() {
         assertEquals(450.5462068965517,
-                new Potassio().calculaQtdAplicarFontePotassio(261.3168, 58)
+                new RecuperacaoPotassio().calculaQtdAplicarFontePotassio(261.3168, 58)
         );
     }
 
     @Test
     public void testaCalculaCustoAplicacao() {
         assertEquals(1126.3655172413792,
-                new Potassio().calculaCustoAplicacao(2500.00, 450.5462068965517)
+                new RecuperacaoPotassio().calculaCustoAplicacao(2500.00, 450.5462068965517)
         );
     }
 
     @Test
     public void testaVerificaTeorOxidoPotassio() {
         assertEquals("Cloreto de Potassio",
-                new Potassio().verificaTeorOxidoPotassio(58));
+                new RecuperacaoPotassio().verificaTeorOxidoPotassio(58));
         assertEquals("Sulfato de Potassio",
-                new Potassio().verificaTeorOxidoPotassio(52));
+                new RecuperacaoPotassio().verificaTeorOxidoPotassio(52));
         assertEquals("Sulfato de Potassio / Magnesio",
-                new Potassio().verificaTeorOxidoPotassio(22)
+                new RecuperacaoPotassio().verificaTeorOxidoPotassio(22)
         );
     }
 
     @Test
     public void testaCalculaTeorAtingir() {
         assertEquals(3.41,
-                new Fosforo().calculaTeorAtingir(8.59, 12.0)
+                new RecuperacaoFosforo().calculaTeorAtingir(8.59, 12.0)
         );
     }
 
     @Test
     public void testaConverteFosforoMgEmKg() {
         assertEquals(6.82,
-                new Fosforo().converteMgEmKg(3.41)
+                new RecuperacaoFosforo().converteMgEmKg(3.41)
         );
     }
 
     @Test
     public void testaPotassioEmPentoxidoDifosforo() {
         assertEquals(15.6178,
-                new Fosforo().potassioEmPentoxidoDifosforo(6.82)
+                new RecuperacaoFosforo().potassioEmPentoxidoDifosforo(6.82)
         );
     }
 
     @Test
     public void testaCalculaPentoxidoDiFosforoNecessario() {
         assertEquals(22.31114285714286,
-                new Fosforo().calculaPentoxidoDiFosforoNecessario(15.6178, 70.0)
+                new RecuperacaoFosforo().calculaPentoxidoDiFosforoNecessario(15.6178, 70.0)
         );
     }
 
     @Test
     public void testaCalculaQtdFosforoAplicar() {
         assertEquals(123.95079365079366,
-                new Fosforo().calculaQtdFosforoAplicar(22.31114285714286)
+                new RecuperacaoFosforo().calculaQtdFosforoAplicar(22.31114285714286)
         );
     }
 
     @Test
     public void testaCusto() {
         assertEquals(156.178,
-                new Fosforo().calculaCusto(1260.0, 123.95079365079366)
+                new RecuperacaoFosforo().calculaCusto(1260.0, 123.95079365079366)
         );
     }
 }
