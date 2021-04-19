ed4f7ebfdcbc866385c501311e6db063474d0ce4
Alysson Cordeiro <Alysson Cordeiro@PC-ALYSSON>
2021-04-19 19:27:06 -0300

    Criação de classe TeordoPentoxidoDifosforo Classe enum para verificação do teor do Pentoxido Difosforo(P2O5) Pois cada fonte de Fosforo possui um teor específico

---
+++ src/main/java/com/mycompany/solo_alysson/TeordoPentoxidoDifosforo.java
@@ -0,0 +1,79 @@
+package com.mycompany.solo_alysson;
+
+public enum TeordoPentoxidoDifosforo {
+
+    SUPERFOSFATO_SIMPLES {
+        @Override
+        public RecuperacaoFosforo calculaTeorPentoxidoDifosforo() {
+            return new RecuperacaoFosforo(18.0);
+        }
+    },
+    SUPERFOSFATO_TRIPLO {
+        @Override
+        public RecuperacaoFosforo calculaTeorPentoxidoDifosforo() {
+            return new RecuperacaoFosforo(41.0);
+        }
+    },
+    MAP {
+        @Override
+        public RecuperacaoFosforo calculaTeorPentoxidoDifosforo() {
+            return new RecuperacaoFosforo(48.0);
+        }
+    },
+    DAP {
+        @Override
+        public RecuperacaoFosforo calculaTeorPentoxidoDifosforo() {
+            return new RecuperacaoFosforo(45.0);
+        }
+    },
+    YOORIN {
+        @Override
+        public RecuperacaoFosforo calculaTeorPentoxidoDifosforo() {
+            return new RecuperacaoFosforo(18.0);
+        }
+    },
+    FOSFATO_NATURAL_DE_ARAD {
+        @Override
+        public RecuperacaoFosforo calculaTeorPentoxidoDifosforo() {
+            return new RecuperacaoFosforo(33.0);
+        }
+    },
+    FOSFATO_GAFSA {
+        @Override
+        public RecuperacaoFosforo calculaTeorPentoxidoDifosforo() {
+            return new RecuperacaoFosforo(29.0);
+        }
+    },
+    FOSFATO_DAOUI {
+        @Override
+        public RecuperacaoFosforo calculaTeorPentoxidoDifosforo() {
+            return new RecuperacaoFosforo(32.0);
+        }
+    },
+    FOSFATO_PATOS_DE_MINAS {
+        @Override
+        public RecuperacaoFosforo calculaTeorPentoxidoDifosforo() {
+            return new RecuperacaoFosforo(24.0);
+        }
+    },
+    ESCORIA_DE_THOMAS {
+        @Override
+        public RecuperacaoFosforo calculaTeorPentoxidoDifosforo() {
+            return new RecuperacaoFosforo(18.5);
+        }
+    },
+    ACIDO_FOSFORICO {
+        @Override
+        public RecuperacaoFosforo calculaTeorPentoxidoDifosforo() {
+            return new RecuperacaoFosforo(52.0);
+        }
+    },
+    MULTIFOSFATO_MAGNESIANO {
+        @Override
+        public RecuperacaoFosforo calculaTeorPentoxidoDifosforo() {
+            return new RecuperacaoFosforo(18.0);
+        }
+    };
+
+    public abstract RecuperacaoFosforo calculaTeorPentoxidoDifosforo();
+}
